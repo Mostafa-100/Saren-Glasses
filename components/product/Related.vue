55f006fec58d85bcc1abf9754bb56f558b3d52c5
Mostafa-100 <imustaphaaourik@gmail.com>
2025-04-23 15:22:38 +0100

    Update product/Related.vue style

--- components/product/Related.vue
+++ components/product/Related.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { useNuxtApp } from '#app';
+import { ref, onMounted } from "vue";
+import { useNuxtApp } from "#app";
 
 const props: any = defineProps({
   item: Object,
@@ -16,16 +16,16 @@ const swiperBreakpoints = {
   320: { slidesPerView: 2 },
   640: { slidesPerView: 3 },
   768: { slidesPerView: 4 },
-  1024: { slidesPerView: 5 },
-  1280: { slidesPerView: 6 },
+  1024: { slidesPerView: 4 },
+  1280: { slidesPerView: 4 },
 };
 
 const fetchRelatedItems = async () => {
   try {
     const ids = props.item.collections.map((c: any) => c._id);
     const { data } = await $storeino.products.search({
-      '_id-ne': props.item._id,
-      'collections._id-in': ids,
+      "_id-ne": props.item._id,
+      "collections._id-in": ids,
     });
 
     items.value = data.results;
@@ -57,7 +57,7 @@ onMounted(() => {
         <!--  -->
         <h3
           v-if="product.related.title.length > 0"
-          class="text-xl lg:text-2xl font-extrabold title-font text-primary"
+          class="text-xl font-light uppercase"
         >
           {{ product.related.title }}
         </h3>
@@ -79,7 +79,7 @@ onMounted(() => {
             <swiper-slide
               v-for="(item, index) in items"
               :key="index"
-              class="h-auto border border-third rounded-2xl overflow-hidden"
+              class="h-auto overflow-hidden"
             >
               <!--  -->
               <ElementProduct :item="item" page="home" />
@@ -99,7 +99,7 @@ onMounted(() => {
         <NuxtLink
           v-if="product.related.more.length > 0"
           to="/shop"
-          class="h-10 w-fit px-8 md:px-16 flex items-center justify-center bg-secondary text-white border border-secondary rounded-full hover:bg-transparent hover:text-secondary transition-all duration-300"
+          class="h-10 w-fit px-8 md:px-16 flex items-center justify-center bg-blue-500 text-white border border-blue-500 rounded-full hover:bg-transparent hover:text-blue-500 transition-all duration-300"
         >
           <!--  -->
           <span class="text-sm font-normal">{{ product.related.more }}</span>
